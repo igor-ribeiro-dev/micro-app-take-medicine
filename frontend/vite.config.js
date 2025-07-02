dea12293b0b0877506f840f7768d151618cee788
Igor Ribeiro <igorribeiro4467@gmail.com>
2025-07-02 00:56:49 -0300

    Configurações para ambiente local

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
   server: {
     proxy: {
       '/medications': 'http://localhost:8080',
       '/config': 'http://localhost:8080',
     },
   },
-});
+}); 
